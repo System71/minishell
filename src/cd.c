57bf77763cadeb8ba390a6afc15ca720bc4df573
Pierre-Antoine Rigaudier <prigaudi@z3r10p6.42lyon.fr>
2025-06-02 17:07:06 +0200

    cd - OK

--- src/cd.c
+++ src/cd.c
@@ -6,7 +6,7 @@
 /*   By: prigaudi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 11:27:48 by prigaudi          #+#    #+#             */
-/*   Updated: 2025/06/02 16:26:30 by prigaudi         ###   ########.fr       */
+/*   Updated: 2025/06/02 17:06:26 by prigaudi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,34 @@ static int	go_home(char ***my_env)
 	return (0);
 }
 
-static int	go_somewhere(char **full_cmd)
+static int	go_last_pwd(char ***my_env)
+{
+	int		i;
+	char	*destination;
+	char	*error_destination;
+
+	i = 0;
+	while ((*my_env)[i])
+	{
+		if (!ft_strncmp(ft_substr((*my_env)[i], 0, 7), "OLDPWD=", 7))
+		{
+			destination = ft_strchr((*my_env)[i], '=') + 1;
+			break ;
+		}
+		i++;
+	}
+	if (chdir(destination))
+	{
+		error_destination = ft_strjoin("minishell: cd: ", destination);
+		perror(error_destination);
+		return (1);
+	}
+	ft_putstr_fd(destination, 1);
+	ft_putstr_fd("\n", 1);
+	return (0);
+}
+
+static int	go_somewhere(char ***my_env, char **full_cmd)
 {
 	char	*destination;
 	char	*error_destination;
@@ -57,6 +84,8 @@ static int	go_somewhere(char **full_cmd)
 		exit_failure("destination malloc ft_strdup");
 		return (1);
 	}
+	if (!ft_strcmp(full_cmd[1], "-"))
+		return (go_last_pwd(my_env));
 	if (chdir(destination))
 	{
 		error_destination = ft_strjoin("minishell: cd: ", destination);
@@ -71,6 +100,6 @@ int	cd(char ***my_env, char **full_cmd)
 	if (!full_cmd[1])
 		return (go_home(my_env));
 	else
-		return (go_somewhere(full_cmd));
+		return (go_somewhere(my_env, full_cmd));
 	return (0);
 }
