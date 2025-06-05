1a2f87a1df0fbff56a8ea9e14c6a8f461686469e
Pierre-Antoine Rigaudier <prigaudi@z3r10p6.42lyon.fr>
2025-06-05 11:42:28 +0200

    MAJ on export and unset

--- src/change_env.c
+++ src/change_env.c
@@ -6,7 +6,7 @@
 /*   By: prigaudi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 13:37:28 by prigaudi          #+#    #+#             */
-/*   Updated: 2025/06/02 18:37:20 by prigaudi         ###   ########.fr       */
+/*   Updated: 2025/06/05 11:37:04 by prigaudi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,44 +67,47 @@ static int	remove_variable(char ***my_env, int position)
 	return (0);
 }
 
-int	unset(char ***my_env, char **full_cmd)
+static int	check_forbidden_char(char *variable_name)
 {
-	char	*variable_to_compare;
-	int		i;
+	int	i;
 
-	if (full_cmd[1] && !ft_strncmp(full_cmd[1], "-", 1))
-	{
-		ft_putstr_fd("minishell: unset : ", 2);
-		ft_putstr_fd(ft_substr(full_cmd[1], 0, 2), 2);
-		ft_putstr_fd(": invalid option\n", 2);
-		return (2);
-	}
 	i = 0;
-	while ((*my_env)[i])
+	while (variable_name[i])
 	{
-		variable_to_compare = extract_variable((*my_env)[i]);
-		if (!ft_strncmp(full_cmd[1], (*my_env)[i], ft_strlen(full_cmd[1])))
-		{
-			remove_variable(my_env, i);
+		if (i == 0 && !ft_isalpha(variable_name[i]) && variable_name[i] != '_')
 			return (0);
-		}
+		if (variable_name[i] == '=')
+			return (1);
+		if (!ft_isalnum(variable_name[i]) && variable_name[i] != '_')
+			return (0);
 		i++;
 	}
 	return (1);
 }
+
 // remove variable if it exists
-static int	check_variable(char ***my_env, char **full_cmd)
+// check forbidden characters
+static int	check_variable(char ***my_env, char *full_cmd)
 {
 	char	*variable_to_add;
 	char	*variable_to_compare;
 	int		i;
 
-	if (!ft_isalpha(*full_cmd[1]))
+	if (*full_cmd == '-')
 	{
-		ft_putstr_fd("export : not a valid identifier\n", 2);
+		ft_putstr_fd("export: ", 2);
+		ft_putstr_fd(ft_substr(full_cmd, 0, 2), 2);
+		ft_putstr_fd(": invalid option\n", 2);
 		return (1);
 	}
-	variable_to_add = extract_variable(full_cmd[1]);
+	if (!check_forbidden_char(full_cmd))
+	{
+		ft_putstr_fd("export : ", 2);
+		ft_putstr_fd(full_cmd, 2);
+		ft_putstr_fd(": not a valid identifier\n\n", 2);
+		return (1);
+	}
+	variable_to_add = extract_variable(full_cmd);
 	i = 0;
 	while ((*my_env)[i])
 	{
@@ -126,24 +129,57 @@ int	export(char ***my_env, char **full_cmd)
 	char	**temp;
 	int		len;
 	int		i;
+	int		j;
 
-	if (check_variable(my_env, full_cmd))
-		return (1);
-	temp = *my_env;
-	len = get_len_env(*my_env);
-	*my_env = malloc(sizeof(char *) * (len + 2));
-	if (!*my_env)
-	{
-		perror("malloc envp");
-		exit(EXIT_FAILURE);
-	}
 	i = 0;
-	while (i < len)
+	while (full_cmd[i])
 	{
-		(*my_env)[i] = strdup(temp[i]);
+		if (check_variable(my_env, full_cmd[i]))
+			return (1);
+		temp = *my_env;
+		len = get_len_env(*my_env);
+		*my_env = malloc(sizeof(char *) * (len + 2));
+		if (!*my_env)
+		{
+			perror("malloc envp");
+			exit(EXIT_FAILURE);
+		}
+		j = 0;
+		while (j < len)
+		{
+			(*my_env)[j] = strdup(temp[j]);
+			j++;
+		}
+		(*my_env)[j++] = strdup(full_cmd[i]);
+		(*my_env)[j] = NULL;
 		i++;
 	}
-	(*my_env)[i++] = strdup(full_cmd[1]);
-	(*my_env)[i] = NULL;
+	return (0);
+}
+
+int	unset(char ***my_env, char **full_cmd)
+{
+	char	*variable_to_compare;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (full_cmd[i])
+	{
+		if (check_variable(my_env, full_cmd[i]))
+			return (1);
+		j = 0;
+		while ((*my_env)[j])
+		{
+			variable_to_compare = extract_variable((*my_env)[j]);
+			if (!ft_strncmp(full_cmd[i], (*my_env)[j], ft_strlen(full_cmd[i])))
+			{
+				remove_variable(my_env, j);
+				return (0);
+			}
+			j++;
+		}
+		i++;
+	}
 	return (0);
 }
